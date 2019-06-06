c61c6d3e1eee849a5f7a109187ae99fb9c364e5c
Leejunxiang-QN1804M <50443869+Leejunxiang-QN1804M@users.noreply.github.com>
2019-06-06 16:51:40 +0800

    Update Jun Xiang PY PA.py

--- Jun Xiang PY PA.py
+++ Jun Xiang PY PA.py
@@ -1,59 +1,41 @@
 class CalUtils:
-
     def __init__(self):
-        self.name = []
-        self.height = 0
+        self.name = ""
+        self.height = 0.00
         self.total_student_count = 0
         self.total_student_height = []
+        self.start = ""
 
     def calAvgHeight(self):
         txtfile = open('listOfStudentHeight.txt', 'r')
-
         for y in txtfile.readlines():
             line = y.split('\t')
-            self.name.append(line[0])
-            n = float(line[1])
+            n = float(line[1].replace("\n", ""))
             self.total_student_height.append(n)
-
-        self.total_student_count = len(self.name)
+            self.total_student_count = self.total_student_count + 1
+        txtfile.close()
 
         avg_height = sum(self.total_student_height) / self.total_student_count
+        print("the average student height is " + str(round(avg_height, 2)) + "m for " + str(self.total_student_count) + "students.")
 
-        txtfile.close()
+        self.name = str(input("enter name: "))
+        self.height = float(input("enter height: "))
 
-        print(round(avg_height, 2))
+        k = open("listOfStudentHeight.txt", "a")
+        k.writelines(str(self.name).upper() + "\t" + str(self.height) + "\n")
+        k.close()
 
-    def main(self):
+        self.total_student_count = 0
+        self.total_student_height = []
 
-        start = input("Do you want to input new student name? Y/N: ").upper()
+        self.start = input("Do you want to input new student name? Y/N: ").upper()
+        if self.start == 'Y':
+            self.calAvgHeight()
+        elif self.start == 'N':
+            print("the average student height is " + str(round(avg_height, 2)) + "m for " + str(self.total_student_count) + "students.")
 
-        if start == 'Y':
-            txtfile = open('listOfStudentHeight.txt', 'a')
-            name = input('Enter a name: ')
-            height = str(input('Enter height: '))
-            txtfile.write = (name + '\t' + height)
-
-        else:
             exit()
-
-        if start == 'N':
-            txtfile = open('listOfStudentHeight.txt', 'r')
-
-        for y in txtfile.readlines():
-            line = y.split('\t')
-            self.name.append(line[0])
-            n = float(line[1])
-            self.total_student_height.append(n)
-
-        self.total_student_count = len(self.name)
-
-        new_avg_height = sum(self.total_student_height) / self.total_student_count
-
-        txtfile.close()
-
-        print(round(new_avg_height, 2))
 
 
 x = CalUtils()
 x.calAvgHeight()
-x.main()
